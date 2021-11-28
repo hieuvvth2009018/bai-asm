32731339891b089b0b52c2798f9c3885f89034a9
hieuvvth2009018 <73455351+hieuvvth2009018@users.noreply.github.com>
2021-11-29 04:50:12 +0700

    hieu nop bai asm cho thay

---
+++ bài assemain/Pages/loginPage.xaml.cs
@@ -0,0 +1,69 @@
+﻿using bài_assemain.Entities;
+using bài_assemain.Services;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace bài_assemain.Pages
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class loginPage : Page
+    {
+        private AccountService AccountService = new AccountService();
+        public loginPage()
+        {
+            this.InitializeComponent();
+            ApplicationView.GetForCurrentView().SetPreferredMinSize(new Size(460, 400));
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            var loginInformation = new LoginInformation()
+            {
+                email = txtEmail.Text,
+                password = txtPassword.Password.ToString()
+            };
+            var credential = await AccountService.LoginAsync(loginInformation);
+            if (credential == null)
+            {
+                ContentDialog contentDialog = new ContentDialog();
+                contentDialog.Title = "Action fails";
+                contentDialog.Content = $"Please try again later!";
+                contentDialog.PrimaryButtonText = "Got it";
+                await contentDialog.ShowAsync();
+            }
+            else
+            {
+                ContentDialog contentDialog = new ContentDialog();
+                contentDialog.Title = "Action success";
+                contentDialog.Content = $"Welcome back";
+                contentDialog.PrimaryButtonText = "Got it";
+                App.CurrentAccount = await AccountService.GetInformationAsync();
+                await contentDialog.ShowAsync();
+                this.Frame.Navigate(typeof(Pages.Demo.NavigationViewDemo));
+            }
+        }
+
+        private void HyperlinkButton_Click(object sender, RoutedEventArgs e)
+        {
+            this.Frame.Navigate(typeof(Pages.RegisterPage));
+        }
+
+    }
+}
